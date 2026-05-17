07ce1290c6c7053594a57ca47dafb5d42bebc65c
Rajesh Kumar <252835131+rajesh-dev-iitkgp@users.noreply.github.com>
2026-05-17 12:00:05 +0530

    Created Interview Model for storing all interviews

---
+++ backend/models/InterviewModel.js
@@ -0,0 +1,32 @@
+const interviewSchema = new mongoose.Schema({
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+    role:String,
+    experienceLevel:String,
+    techStack:[String],
+    interviewType:String,
+    totalScore:Number,
+    overallFeedback:{
+        strengths:[String],
+        weaknesses:[String],
+        idealApproach:[String]
+    },
+    questions:[
+        {
+            question:String,
+            userAnswer:String,
+            score:Number,
+            feedback: {
+                strengths: [String],
+                weaknesses: [String],
+                idealApproach: [String],
+            },
+        }
+    ],
+},{timestamps:true})
+
+const InterviewModel = mongoose.models.Interview || new mongoose.model("Interviews",interviewSchema)
+
+export default InterviewModel
